0a1afb7a1adb9fb10fba1fdb850e2e4d3800f89c
chichun <chichunhuamg@gmail.com>
2023-10-31 12:35:06 +0800

    project config

--- burning/docusaurus.config.js
+++ burning/docusaurus.config.js
@@ -11,15 +11,15 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://chichunhuang.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/burning',
+  baseUrl: '/burning/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Burny', // Usually your GitHub org/user name.
-  projectName: 'Burning', // Usually your repo name.
+  organizationName: 'chichunhuang', // Usually your GitHub org/user name.
+  projectName: 'burning', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
